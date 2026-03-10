1bc6351f906acd629545c7c2b25d05f933a57029
Vinicius Henrique <vnshas@gmail.com>
2026-03-10 18:28:31 -0300

    Adicionado mudança de tema salvando no localStorage e mudando icone botao menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,45 @@
 
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon,} from 'lucide-react'
 import styles from './style.module.css'
+import { useEffect, useState } from 'react'
 
-
+type AvaiableThemes = 'dark' | 'light'
 
 export const Menu = () =>{
+   
+    const [theme, setTheme] = useState<AvaiableThemes>(() =>{
+    
+        const storageTheme = localStorage.getItem('theme') as AvaiableThemes || 'dark'
+
+        return storageTheme
+    })  
+    
+    function handleTheme(event : React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        event.preventDefault()
+
+        setTheme(prevTheme =>{
+            const nextTheme =  prevTheme === 'dark' ? 'light' : 'dark'
+            return nextTheme
+        })
+    }
+    
+    useEffect(() =>{
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+    },[theme])
+    
     return(
         <nav className={styles.menu}>
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Home' title='Home'>
                 <HouseIcon />   
             </a>
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Historico' title='Historico'>
                 <HistoryIcon />   
             </a>
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Configurações' title='Configurações'>
                 <SettingsIcon />   
             </a>
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Mudar Tema' title='Mudar' onClick={handleTheme}>
                 <SunIcon />   
             </a>
         </nav>
