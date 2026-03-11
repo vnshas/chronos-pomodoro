6ef75c2d64d4b39ec41738335a0931c6c81051bb
Vinicius Henrique <vnshas@gmail.com>
2026-03-11 20:25:36 -0300

    Criada função getNextCycle e implementado no MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,17 +2,63 @@ import { PlayCircleIcon } from "lucide-react"
 import { Cycles } from "../Cycles"
 import { DefaultButton } from "../DefaultButton"
 import { DefaultInput } from "../DefaultInput"
+import { useRef } from "react"
+import type { TaskModel } from "../../models/TaskModel"
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
+import { getNextCycle } from "../../utils/getNextCycle"
 
 export const MainForm = () =>{
-    return(
-        <form className="form" action="">
+  const {state,setState} = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement >(null)
+
+  //ciclos
+  const nextCycle = getNextCycle(state.currentCycle)
+  console.log(nextCycle)
+  
+  function handleCreateNewTask(event:React.SubmitEvent<HTMLFormElement>){
+    event.preventDefault()
+    if (taskNameInput.current === null) return
+
+    const taskName = taskNameInput.current.value.trim()
+
+    if(!taskName){
+      alert('Valor inválido')
+      return
+    }
+    const newTask : TaskModel = {
+        id: Date.now.toString(),
+        name: taskName,
+        duration: 1,
+        startDate: Date.now(),
+        completeDate: null,
+        interruptDate: null, 
+        type: "workTime",
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState =>{
+      return {
+        ...prevState,
+        config:{...prevState.config},
+        activeTask: newTask,
+        currentCycle:nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+  }  
+  
+  return(
+        <form onSubmit={ handleCreateNewTask} className="form" action="">
           <div className="formRow">
             <DefaultInput
               labelText="task"
               id="input"
               type="text"
               placeholder="Digite Algo"
-            
+              ref={taskNameInput}
             />
           </div>
 
