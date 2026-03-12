dbe14856508df11aa17b006df976eaf9476edfba
Vinicius Henrique <vnshas@gmail.com>
2026-03-12 14:42:21 -0300

    Adicionoda função getNextCycle e implementada em MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,6 +6,7 @@ import { useRef } from "react"
 import type { TaskModel } from "../../models/TaskModel"
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext"
 import { getNextCycle } from "../../utils/getNextCycle"
+import { getNextCycleType } from "../../utils/getNextCycleType"
 
 export const MainForm = () =>{
   const {state,setState} = useTaskContext()
@@ -14,6 +15,7 @@ export const MainForm = () =>{
   //ciclos
   const nextCycle = getNextCycle(state.currentCycle)
   console.log(nextCycle)
+  const nextCycleType = getNextCycleType(nextCycle)
   
   function handleCreateNewTask(event:React.SubmitEvent<HTMLFormElement>){
     event.preventDefault()
@@ -32,7 +34,7 @@ export const MainForm = () =>{
         startDate: Date.now(),
         completeDate: null,
         interruptDate: null, 
-        type: "workTime",
+        type: nextCycleType,
     }
 
     const secondsRemaining = newTask.duration * 60
