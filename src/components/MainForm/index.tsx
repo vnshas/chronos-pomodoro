cd4c92a6f48d921b746d2cae45959c17221c61b1
Vinicius Henrique <vnshas@gmail.com>
2026-03-12 19:02:43 -0300

    Desativando o input se tem task ativa (disabled) e Trocando o botão de iniciar para parar tarefa atual

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,4 +1,4 @@
-import { PlayCircleIcon } from "lucide-react"
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react"
 import { Cycles } from "../Cycles"
 import { DefaultButton } from "../DefaultButton"
 import { DefaultInput } from "../DefaultInput"
@@ -10,49 +10,49 @@ import { getNextCycleType } from "../../utils/getNextCycleType"
 import { formatSecondsTominutes } from "../../utils/formatSecondsToMinutes"
 
 export const MainForm = () =>{
-  const {state,setState} = useTaskContext()
-  const taskNameInput = useRef<HTMLInputElement >(null)
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   //ciclos
-  const nextCycle = getNextCycle(state.currentCycle)
-  console.log(nextCycle)
-  const nextCycleType = getNextCycleType(nextCycle)
-  
-  function handleCreateNewTask(event:React.SubmitEvent<HTMLFormElement>){
-    event.preventDefault()
-    if (taskNameInput.current === null) return
+  const nextCycle = getNextCycle(state.currentCycle);
+  console.log(nextCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
-    const taskName = taskNameInput.current.value.trim()
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (taskNameInput.current === null) return;
 
-    if(!taskName){
-      alert('Valor inválido')
-      return
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Valor inválido");
+      return;
     }
-    const newTask : TaskModel = {
-        id: Date.now.toString(),
-        name: taskName,
-        duration: state.config[nextCycleType],
-        startDate: Date.now(),
-        completeDate: null,
-        interruptDate: null, 
-        type: nextCycleType,
-    }
+    const newTask: TaskModel = {
+      id: Date.now.toString(),
+      name: taskName,
+      duration: state.config[nextCycleType],
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: nextCycleType,
+    };
 
-    const secondsRemaining = newTask.duration * 60
+    const secondsRemaining = newTask.duration * 60;
 
-    setState(prevState =>{
+    setState((prevState) => {
       return {
         ...prevState,
-        config:{...prevState.config},
+        config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle:nextCycle,
+        currentCycle: nextCycle,
         secondsRemaining,
         formattedSecondsRemaining: formatSecondsTominutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask]
-      }
-    })
-  }  
-  
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
     <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
@@ -62,6 +62,7 @@ export const MainForm = () =>{
           type="text"
           placeholder="Digite Algo"
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
@@ -76,7 +77,22 @@ export const MainForm = () =>{
       )}
 
       <div className="formRow">
-        <DefaultButton icon={<PlayCircleIcon />} />
+        {!state.activeTask ? (
+          <DefaultButton
+            aria-label="Iniciar nova tarefa"
+            title="Iniciar nova tarefa"
+            type="submit"
+            icon={<PlayCircleIcon />}
+          />
+        ) : (
+          <DefaultButton
+            aria-label="Interromper tarefa"
+            title="Interromper tarefa"
+            type="button"
+            color="red"
+            icon={<StopCircleIcon />}
+          />
+        )}
       </div>
     </form>
   );
